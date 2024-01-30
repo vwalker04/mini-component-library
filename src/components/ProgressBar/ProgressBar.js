378f60d1f676ef11e2a281291d458e1b886e8c07
Vaughn Walker <vwalk04@gmail.com>
2024-01-30 11:24:22 -0500

    Create progress bar with prop passing for styles

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,11 +2,67 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import {COLORS} from '../../constants';
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const STYLES = {
+    small: {
+        height: 8,
+        padding: 0,
+        radius: 4
+    },
+    medium: {
+        height: 12,
+        padding: 0,
+        radius: 4
+    },
+    large: {
+        height: 16,
+        padding: 4,
+        radius: 8
+    }
+}
+
+const ProgressBar = ({value, size}) => {
+    const styles = STYLES[size];
+
+    if (!styles) {
+        throw new Error("Unknown sized passed to progress bar.")
+    }
+
+    return (
+        <Wrapper
+            role={"progressbar"}
+            aria-valuemin={0}
+            aria-valuemax={100}
+            aria-valuenow={value}
+            padding={styles.padding}
+            radius={styles.radius}
+        >
+            <VisuallyHidden>{value}%</VisuallyHidden>
+            <BarWrapper>
+                <Bar value={value} height={styles.height}/>
+            </BarWrapper>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+    border-radius: ${props => props.radius + "px"};
+    padding: ${props => props.padding + "px"}
+`
+
+const BarWrapper = styled.div`
+    border-radius: 4px;
+    overflow: hidden;
+`
+
+const Bar = styled.div`
+    width: ${props => props.value + "%"};
+    height: ${props => props.height + "px"};
+    background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
